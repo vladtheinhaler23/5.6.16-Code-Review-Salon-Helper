4ae6662f9cb50d456bce35655dde856d79c37063
Joshua Logan <joshlogan32323@gmail.com>
2016-05-08 14:51:33 -0700

    intial commit with basic file structure

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,25 @@
+import org.fluentlenium.adapter.FluentTest;
+import static org.junit.Assert.*;
+import org.junit.*;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.sql2o.*;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import static org.fluentlenium.core.filter.FilterConstructor.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest extends FluentTest {
+  public WebDriver webDriver = new HtmlUnitDriver();
+
+  @Override
+   public WebDriver getDefaultDriver() {
+     return webDriver;
+   }
+
+   @ClassRule
+   public static ServerRule server = new ServerRule();
+
+
+}
---
+++ src/test/java/StylistTest.java
@@ -0,0 +1,8 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.sql2o.*;
+import java.util.Arrays;
+
+public class StylistTest {
+
+}
